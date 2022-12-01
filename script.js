e0f4155633dee5ef658b55c3b1c54319430eef8f
Dreizer <89171351+Dreizerr@users.noreply.github.com>
2022-12-01 22:35:15 +0200

    Меню Бургер

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let menu = document.querySelector(".menu");
 let burger = document.querySelector(".menu-burger");
+let menuItems = document.querySelectorAll(".menu__item");
+let wrapper = document.querySelector(".wrapper");
+let main = document.querySelector(".main");
 
 burger.onclick = function () {
   menu.classList.toggle("active");
@@ -8,4 +11,16 @@ burger.onclick = function () {
   } else {
     burger.classList.remove("active");
   }
+};
+
+for (let menuItem of menuItems) {
+  menuItem.onclick = function () {
+    menu.classList.remove("active");
+    burger.classList.remove("active");
+  };
+}
+
+main.onclick = function () {
+  menu.classList.remove("active");
+  burger.classList.remove("active");
 };
